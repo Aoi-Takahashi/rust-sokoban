7fb3850c0aa9bb7140780edc29cb7be1dfe499f7
Aoi-Takahashi <pythondevelop.forbusines@gmail.com>
2025-01-21 23:00:35 +0900

    chore:Project setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use ::ggez::{conf, event, Context, GameResult};
+use ::std::path;
+
+struct Game {}
+
+// This is the main event loop. ggez tells us to implement
+// two things: 1.updating 2.rendering
+impl event::EventHandler<ggez::GameError> for Game {
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), ggez::GameError> {
+        // TODO: update game logic here
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut Context) -> Result<(), ggez::GameError> {
+        // TODO: update draw here
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    // Create a game context and event loop
+    let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
+        .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
+        .window_mode(conf::WindowMode::default().dimensions(1500.0, 1500.0))
+        .add_resource_path(path::PathBuf::from("./resources"));
+
+    let (context, event_loop) = context_builder.build()?;
+    // Create the game state
+
+    let game = Game {};
+    // Run the main event loop
+    event::run(context, event_loop, game)
 }
