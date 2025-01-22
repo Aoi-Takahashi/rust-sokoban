5397b27f24fd9783a45ebc0c92ab72f7d7b16caf
Aoi-Takahashi <pythondevelop.forbusines@gmail.com>
2025-01-23 01:56:42 +0900

    chore: Components and entities

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,77 @@
 use ::ggez::{conf, event, Context, GameResult};
 use ::std::path;
+use hecs::{Entity, World};
 
 struct Game {}
 
+#[allow(dead_code)]
+pub struct Position {
+    x: u8,
+    y: u8,
+    z: u8,
+}
+
+#[allow(dead_code)]
+pub struct Renderable {
+    path: String,
+}
+
+pub struct Wall {}
+
+pub struct Player {}
+
+pub struct Box {}
+
+pub struct BoxSpot {}
+
+pub fn create_wall(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/wall.png".to_string(),
+        },
+        Wall {},
+    ))
+}
+
+pub fn create_floor(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 5, ..position },
+        Renderable {
+            path: "/images/floor.png".to_string(),
+        },
+    ))
+}
+
+pub fn create_box(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/box.png".to_string(),
+        },
+        Box {},
+    ))
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 9, ..position },
+        Renderable {
+            path: "/images/box_spot.png".to_string(),
+        },
+        BoxSpot {},
+    ))
+}
+
+pub fn create_player(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/player.png".to_string(),
+        },
+        Player {},
+    ))
+}
 // This is the main event loop. ggez tells us to implement
 // two things: 1.updating 2.rendering
 impl event::EventHandler<ggez::GameError> for Game {
